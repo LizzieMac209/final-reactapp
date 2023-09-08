b85d711d082360ce771ca8c148fefe507e011777
LizzieMac209 <Emacfarlane05@gmail.com>
2023-09-07 20:04:39 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://lizziemacfarlane-frntenddvlprprtflio.netlify.app/"
-          target="_blank"
-        >
-          Lizzie Macfarlane
-        </a>{" "}
-        & is 🪷{" "}
-        <a
-          href="https://github.com/LizzieMac209/final-reactapp"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://lizziemacfarlane-frntenddvlprprtflio.netlify.app/"
+            target="_blank"
+          >
+            Lizzie Macfarlane
+          </a>{" "}
+          & is 🪷{" "}
+          <a
+            href="https://github.com/LizzieMac209/final-reactapp"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
